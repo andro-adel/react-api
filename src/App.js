31eada58fc4b0effd5c88097cf1635865307a930
andro <androadel699@gmail.com>
2021-03-21 19:42:19 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { x as f, y, Test } from "./module";
+import Test2 from "./module";
+import { default as app } from "./module";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello React {f + y}
+      {Test()}
+      {Test2()}
+      {app()}
     </div>
   );
 }
---
+++ src/module.js
@@ -0,0 +1,13 @@
+export const x = 2;
+
+export const y = 8;
+
+export function Test() {
+  console.log("React Js");
+}
+
+function Test2() {
+  console.log("Default React Js");
+}
+
+export default Test2;
