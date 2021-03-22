87668a511d32ded4c5b6db5f0763472604a32c2d
andro <androadel699@gmail.com>
2021-03-22 17:00:16 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,28 @@ class App extends Component {
 
   Testo = (p) => console.log("Test !!!@@");
 
+  state = {
+    name: "Hamza",
+    job: "doctor",
+    age: 25,
+  };
+
+  HandleClickState() {
+    console.log(this.state.name);
+  }
+
+  HandleClickState2 = () => console.log(this.state.job);
+
+  HandleClick() {
+    console.log("Clicked");
+  }
+
+  HandleMouseMove() {
+    console.log("MouseMove");
+  }
+
+  change = () => {};
+
   render() {
     return (
       <div className="App">
@@ -22,6 +44,11 @@ class App extends Component {
         {app()}
         <Child />
         {this.Testo()}
+        <button onClick={this.HandleClickState.bind(this)}>Click This</button>
+        <button onClick={this.HandleClickState2}>Click This 2</button>
+        <button onClick={this.HandleClick}>Click</button>
+        <button onMouseMove={this.HandleMouseMove}>MouseMove</button>
+        <button onClick={this.change}>Change State</button>
       </div>
     );
   }
