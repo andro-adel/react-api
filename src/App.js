2e40525701c060f38fe0c9944f390786aaec7d9b
andro <androadel699@gmail.com>
2021-03-22 14:44:18 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import React, { Component } from "react";
 import { x as f, y, Test } from "./module";
 import Test2 from "./module";
 import { default as app } from "./module";
-function App() {
-  return (
-    <div className="App">
-      Hello React {f + y}
-      {Test()}
-      {Test2()}
-      {app()}
-    </div>
-  );
+import Child from "./Child";
+
+class App extends Component {
+  Testo() {
+    console.log("Test !");
+  }
+
+  Testo = () => console.log("Test !!!");
+
+  Testo = (p) => console.log("Test !!!@@");
+
+  render() {
+    return (
+      <div className="App">
+        Hello React {f + y}
+        {Test()}
+        {Test2()}
+        {app()}
+        <Child />
+        {this.Testo()}
+      </div>
+    );
+  }
 }
 
 export default App;
